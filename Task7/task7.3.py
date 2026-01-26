32c050a49e2cd911ccc92d7b96b77a63e283787d
yuvraj006-byte <yuvivermasep006@gmail.com>
2026-01-26 18:36:57 +0200

    Python Module 7

--- Task7/task7.3.py
+++ Task7/task7.3.py
@@ -7,7 +7,9 @@ def airport_info():
         print("A: Do you want to enter a new airport? OR")
         print("B: Do you want to fetch the information of an existing airport?")
         print("Press ENTER to quit!")
+        print()
         user_input = str(input("Which type of service would you prefer? (A or B): "))
+        print()
         if user_input == "A":
             airport_name_info = str(input("Please enter the name of the airport: "))
             airport_code_info = str(input("Please enter the ICAO code of the airport: "))
@@ -15,21 +17,27 @@ def airport_info():
             if airport_code_info in airports:
                 if airport_code_info in airports and airports[airport_code_info] == airport_name_info:
                    print(f"{airport_name_info} exists in our database already!")
+                   print()
                 else:
                    print(f"{airport_code_info} exists but with a different name!")
+                   print()
             else:
                 airports[airport_code_info] = airport_name_info
                 print(f"{airport_name_info} added to the database!")
+                print()
 
         elif user_input == "B":
-            user_input_code = str(input(" Alright! Please enter the ICAO code of the airport: "))
+            user_input_code = str(input("Alright! Please enter the ICAO code of the airport: "))
             if user_input_code in airports:
                 print(f"The Airport you were looking for is: {airports[user_input_code]}!")
+                print()
             else:
                 print(f"{user_input_code} does not exist in the database!")
+                print()
 
         else:
             print(f"Airports that exist in our database are: {airports}")
+            print()
             break
 
 airport_info()
